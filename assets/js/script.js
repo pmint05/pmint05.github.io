d47e3791bba077d4e2163646ea2d3d4bc5555f76
Pmint 05 <phucon547@gmail.com>
2021-10-10 19:59:31 +0700

    update block dev mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,40 @@
 const introContainer = document.querySelector(".container");
 
-
 $(document).ready(() => {
 	// lock scroll position, but retain settings for later
 	var scrollPosition = [
-		self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-		self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+		self.pageXOffset ||
+			document.documentElement.scrollLeft ||
+			document.body.scrollLeft,
+		self.pageYOffset ||
+			document.documentElement.scrollTop ||
+			document.body.scrollTop,
 	];
-	var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
-	html.data('scroll-position', scrollPosition);
-	html.data('previous-overflow', html.css('overflow'));
-	html.css('overflow', 'hidden');
+	var html = jQuery("html"); // it would make more sense to apply this to body, but IE7 won't have that
+	html.data("scroll-position", scrollPosition);
+	html.data("previous-overflow", html.css("overflow"));
+	html.css("overflow", "hidden");
 	window.scrollTo(scrollPosition[0], scrollPosition[1]);
-	
+
 	//Get theme from localstorage
 	var theme = localStorage.getItem("theme");
-	if(theme != "dark"){
+	if (theme != "dark") {
 		body.classList.add("dark");
-	}else{
+	} else {
 		body.classList.remove("dark");
-	};
+	}
 });
 
-function endIntro(){
-	setTimeout(function(){
+function endIntro() {
+	setTimeout(function () {
 		introContainer.style.opacity = "0";
 		setTimeout(() => {
 			// un-lock scroll position
-			var html = jQuery('html');
-			var scrollPosition = html.data('scroll-position');
-			html.css('overflow', html.data('previous-overflow'));
+			var html = jQuery("html");
+			var scrollPosition = html.data("scroll-position");
+			html.css("overflow", html.data("previous-overflow"));
 			window.scrollTo(scrollPosition[0], scrollPosition[1]);
-			
-			
+
 			$(".header").css("opacity", "1");
 			$(".logo").fadeIn();
 			$(".home-btn").css("opacity", "1");
@@ -43,123 +45,124 @@ function endIntro(){
 			$(".about").css("opacity", "1");
 			$(".contact").css("opacity", "1");
 			$(".projects").css("opacity", "1");
-			if(screen.width <= 700){
+			if (screen.width <= 700) {
 				$(".hamburger").css("display", "flex");
-			}else{
+			} else {
 				$(".hamburger").css("display", "none");
-			};
+			}
 			setTimeout(() => {
 				$(".love-container").removeClass("hide");
-				
+
 				$("FOOTER").css("display", "flex");
 				type();
-				
 			}, 1000);
 			$(".logo-img").css("display", "flex");
 			$(".theme-btn").css("display", "flex");
-			introContainer.addEventListener("transitionend", () => {
-				introContainer.style.display = "none";
-			}, 1000);
+			introContainer.addEventListener(
+				"transitionend",
+				() => {
+					introContainer.style.display = "none";
+				},
+				1000
+			);
 		}, 1000);
 	}, 8000);
-};
+}
 
-
-jQuery(function($) {
-	
+jQuery(function ($) {
 	// MAD-RIPPLE // (jQ+CSS)
-	$(document).on("mousedown", "[data-ripple]", function(e) {
-		
+	$(document).on("mousedown", "[data-ripple]", function (e) {
 		var $self = $(this);
-		
-		if($self.is(".btn-disabled")) {
+
+		if ($self.is(".btn-disabled")) {
 			return;
 		}
-		if($self.closest("[data-ripple]")) {
-			
+		if ($self.closest("[data-ripple]")) {
 		}
-		
+
 		var initPos = $self.css("position"),
-		offs = $self.offset(),
-		x = e.pageX - offs.left,
-		y = e.pageY - offs.top,
-		dia = Math.min(this.offsetHeight, this.offsetWidth, 100), // start diameter
-		$ripple = $('<div/>', {class : "ripple",appendTo : $self });
-		
-		if(!initPos || initPos==="static") {
-			$self.css({position:"relative"});
+			offs = $self.offset(),
+			x = e.pageX - offs.left,
+			y = e.pageY - offs.top,
+			dia = Math.min(this.offsetHeight, this.offsetWidth, 100), // start diameter
+			$ripple = $("<div/>", { class: "ripple", appendTo: $self });
+
+		if (!initPos || initPos === "static") {
+			$self.css({ position: "relative" });
 		}
-		
-		$('<div/>', {
-		class : "rippleWave",
-		css : {
-			background: $self.data("ripple"),
-			width: dia,
-			height: dia,
-			left: x - (dia/2),
-			top: y - (dia/2),
-		},
-		appendTo : $ripple,
-		one : {
-			animationend : function(){
-				$ripple.remove();
-			}
-		}
-	});
+
+		$("<div/>", {
+			class: "rippleWave",
+			css: {
+				background: $self.data("ripple"),
+				width: dia,
+				height: dia,
+				left: x - dia / 2,
+				top: y - dia / 2,
+			},
+			appendTo: $ripple,
+			one: {
+				animationend: function () {
+					$ripple.remove();
+				},
+			},
+		});
 	});
 });
 
-function shift(){
+function shift() {
 	document.querySelector(".skills-bars").classList.add("animated");
 	setTimeout(() => {
 		document.querySelector(".skills-head").classList.add("animated");
 	}, 1500);
 }
 
-
-
 //Disable right click
-document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener("contextmenu", (event) => event.preventDefault());
 
 //TYPE.JS
-function type(){
-	let typed = new Typed('#typed', {
-		strings: ["Beginning Web Deverloper", "Beginning Graphic Designer", "Student."],
+function type() {
+	let typed = new Typed("#typed", {
+		strings: [
+			"Beginning Web Deverloper",
+			"Beginning Graphic Designer",
+			"Student.",
+		],
 		typeSpeed: 70,
 		backSpeed: 20,
 		backDelay: 500,
 		smartBackspace: false,
 		loop: false,
-		onComplete: function() {
+		onComplete: function () {
 			shift();
 			typed.loop = true;
-		} 
+		},
 	});
 	typed.stop();
 	//ANIMATION ON SCROLL
-	$(window).scroll(function() {
-		var hT = $('.text-body').offset().top,
-		hH = $('.text-body').outerHeight(),
-		wH = $(window).height(),
-		wS = $(this).scrollTop();
-		if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+	$(window).scroll(function () {
+		var hT = $(".text-body").offset().top,
+			hH = $(".text-body").outerHeight(),
+			wH = $(window).height(),
+			wS = $(this).scrollTop();
+		if (wS > hT + hH - wH && hT > wS && wS + wH > hT + hH) {
 			typed.start();
 		} else {
 			typed.stop();
 		}
 	});
-};
+}
 
-$(window).scroll(function() {
-	const 	wH = $(window).height();
-	const	wS = $(window).scrollTop();
-	var 	hT = $('.text-body').offset().top,
-			hH = $('.text-body').outerHeight();
+$(window).scroll(function () {
+	const wH = $(window).height();
+	const wS = $(window).scrollTop();
+	var hT = $(".text-body").offset().top,
+		hH = $(".text-body").outerHeight();
 
-	if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+	if (wS > hT + hH - wH && hT > wS && wS + wH > hT + hH) {
 		setTimeout(() => {
-			$('.about-head').css("opacity", "1");
-			$('.text-body').delay(1000).css("opacity", "1");
+			$(".about-head").css("opacity", "1");
+			$(".text-body").delay(1000).css("opacity", "1");
 		}, 500);
 	} else {
 		// $('.about-head').css("opacity", "0");
@@ -176,53 +179,66 @@ function toggleLoop(typed) {
 }
 
 function validate() {
-    const firstName = document.querySelector(".first-name");
-    const lastName = document.querySelector(".last-name");
-    const email = document.querySelector(".email-add");
-    const message = document.querySelector(".message");
+	const firstName = document.querySelector(".first-name");
+	const lastName = document.querySelector(".last-name");
+	const email = document.querySelector(".email-add");
+	const message = document.querySelector(".message");
 	var emailID = email.value;
-		atpos = emailID.indexOf("@");
-		dotpos = emailID.lastIndexOf(".");
-	
-	if(firstName.value == "" ) {
+	atpos = emailID.indexOf("@");
+	dotpos = emailID.lastIndexOf(".");
+
+	if (firstName.value == "") {
 		firstName.focus();
 		Swal.fire({
-			title: 'Error!',
-			text: 'Please provide your first name.',
-			icon: 'error',
-			confirmButtonText: 'Ok'
+			title: "Error!",
+			text: "Please provide your first name.",
+			icon: "error",
+			confirmButtonText: "Ok",
 		});
-		return(false);
+		return false;
 	}
-	if(lastName.value == "" ) {
+	if (lastName.value == "") {
 		lastName.focus();
 		Swal.fire({
-			title: 'Error!',
-			text: 'Please provide your last name.',
-			icon: 'error',
-			confirmButtonText: 'Ok'
+			title: "Error!",
+			text: "Please provide your last name.",
+			icon: "error",
+			confirmButtonText: "Ok",
 		});
-		return(false);
+		return false;
 	}
-	if(email.value == "" || atpos < 1 || ( dotpos - atpos < 2 )) {
+	if (email.value == "" || atpos < 1 || dotpos - atpos < 2) {
 		email.focus();
 		Swal.fire({
-			title: 'Error!',
-			text: 'Please provide valid email.',
-			icon: 'error',
-			confirmButtonText: 'Ok'
+			title: "Error!",
+			text: "Please provide valid email.",
+			icon: "error",
+			confirmButtonText: "Ok",
 		});
-		return(false);
+		return false;
 	}
-	if(message.value == "") {
+	if (message.value == "") {
 		message.focus();
 		Swal.fire({
-			title: 'Error!',
-			text: 'Please say something.',
-			icon: 'error',
-			confirmButtonText: 'Ok'
+			title: "Error!",
+			text: "Please say something.",
+			icon: "error",
+			confirmButtonText: "Ok",
 		});
-		return(false);
+		return false;
 	}
-	return(true);
+	return true;
 }
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+$(document).keydown(function (event) {
+	if (event.keyCode == 123) {
+		return false;
+	} else if (
+		(event.ctrlKey && event.shiftKey && event.keyCode == 73) ||
+		(event.ctrlKey && event.shiftKey && event.keyCode == 67) ||
+		(event.ctrlKey && event.shiftKey && event.keyCode == 74) ||
+		(event.ctrlKey && event.keyCode == 85)
+	) {
+		return false;
+	}
+});
